fdab63f630b2c04a23049fcd131e02dc7bff4bc1
dallgoot <dallgoot@users.noreply.github.com>
2023-11-19 22:21:56 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Merci ! de ta curiosité 😊</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          ..mais je n'ai pas le physique pour un Onlyfans 😘
         </p>
       </main>
 
